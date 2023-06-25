dfaea795bb1db7034ccde2ed4ce55ef2a24e974a
Abhhhinav <107941084+Abhhhinav@users.noreply.github.com>
2023-06-26 02:16:21 +0530

    Add files via upload

---
+++ SearchBST.java
@@ -0,0 +1,50 @@
+package BinarySearchTrees;
+public class SearchBST {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+        if (root.data > val) {
+            root.left = insert(root.left, val);
+        } else {
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+    public static boolean SEARCH(Node root, int key) {
+        if (root == null) {
+            return false;
+        }
+        if (root.data == key) {
+            return true;
+        }
+        if (root.data > key) {
+            return SEARCH(root.left, key);
+        } else {
+            return SEARCH(root.right, key);
+        }
+    }
+
+    public static void main(String[] args) {
+        int values[] = { 5, 1, 3, 4, 2, 7 };
+        Node root = null;
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+        if (SEARCH(root, 10)) {
+            System.out.println("FOund");
+        } else {
+            System.out.println("Not found");
+        }
+    }
+}
